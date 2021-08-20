87b10438292e7fe77b0df3b4a4647aa232fad322
TheCreeperAPI <36825697+TheCreeperAPI@users.noreply.github.com>
2021-08-20 12:43:57 -0700

    :heavy_plus_sign: :heavy_minus_sign: Replacing gson with org.json

--- src/main/java/ca/shenru/StockTracking/Credentials.java
+++ src/main/java/ca/shenru/StockTracking/Credentials.java
@@ -1,4 +1,8 @@
 package ca.shenru.StockTracking;
 
 public class Credentials {
+    public static boolean VerifyConfig(){
+        //TODO: Actually implement code here lol
+        return true;
+    }
 }
