9f35aba083059ef692af809c7dacf699175da98f
TheCreeperAPI <36825697+TheCreeperAPI@users.noreply.github.com>
2021-08-21 16:16:11 -0700

    :bug: Fixed a stupid null-pointer exception error because apparently the fricking getDataset thing gets 2 rows of garbage.

--- src/main/java/ca/shenru/StockTracking/Main.java
+++ src/main/java/ca/shenru/StockTracking/Main.java
@@ -23,16 +23,19 @@ public class Main {
         String DatasetPath = Configurations.getConfig("DatasetLocation");
         //Get the data
         String[][] rawData = DataManager.getData(DatasetPath);
+
         String Description;
         float[] temp;
         float[][] SentimentScore = new float[2][parseInt(Configurations.getConfig("DatasetEntries"))];
-        for (int i = 0; i < rawData.length; i++){
+        for (int i = 2; i < rawData.length; i++){
             Description = rawData[1][i];
+            System.out.println("[DEBUG] Verifying dataset path: "+DatasetPath);
+            System.out.println("[INFO] Starting to analyze "+Description);
             temp = Sentiments.analyze(Description);
 
             rawData[0][i] = String.valueOf(temp[0]);
             rawData[1][i] = String.valueOf(temp[1]);
-            System.out.println("[Info] Finished analyzing "+1+". Results are: " +String.valueOf(temp[0])+ " and "+String.valueOf(temp[1]));
+            System.out.println("[Info] Finished analyzing "+i+". Results are: " +String.valueOf(temp[0])+ " and "+String.valueOf(temp[1]));
         }
 
 
